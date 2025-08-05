f310f5e20beed28faede4ba8eb6150ac7093071d
guidoScotti <scottigui@gmail.com>
2025-08-05 14:37:13 -0300

    modal added

--- main.js
+++ main.js
@@ -20,7 +20,26 @@ prevBtn.addEventListener('click', () => {
   showImage(current);
 });
 
-// Mostrar la imagen inicial
 showImage(current);
 
 /// ---- AGRANDADO DE IMAGEN ----
+  const modal = document.getElementById("imagen-modal");
+  const modalImg = document.getElementById("img-ampliada");
+  const cerrar = document.querySelector(".cerrar");
+
+  document.querySelectorAll('.menu-carillas img').forEach(img => {
+    img.addEventListener('click', () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+    });
+  });
+
+  cerrar.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  modal.onclick = function(e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  }
